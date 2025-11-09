b6dc1fb61cfff95291930ecfcd668f54ad0cc73a
PAWAN TRIPATHI <pawantripathi802@gmail.com>
2025-11-09 07:26:20 +0530

    9 TH NOV

---
+++ SQL COMMANDS TYPE.sql
@@ -0,0 +1,31 @@
+--sql commands types 
+
+--1- DDL - data defination lanuage
+-- use to define the structure of the database( tables , schemas )
+
+--alter
+--truncate
+-- create
+--drop 
+
+
+--2- DQL - data query language
+-- use to fetch the data 
+-- select
+
+--3- DML data manupilation language
+
+-- insert
+--update
+--delete
+
+--4- DCL data control language
+
+-- grant 
+-- revoke
+
+--5- TCL transaction control language
+
+--commit 
+-- rollback 
+--save point
