c57b0a2a1c452364cbfff4739da25bba31532537
PAWAN TRIPATHI <pawantripathi802@gmail.com>
2025-11-11 20:43:29 +0530

    Add files via upload

---
+++ LAST & FIRST VALUE FUNCTION .sql
@@ -0,0 +1,88 @@
+CREATE TABLE EmployeeSalaries (
+    EmployeeID INT,
+    EmployeeName VARCHAR(50),
+    Salary INT,
+    Department VARCHAR(50)
+);
+
+
+INSERT INTO EmployeeSalaries (EmployeeID, EmployeeName, Salary, Department)
+VALUES
+(1, 'Alice', 50000, 'HR'),
+(2, 'Bob', 60000, 'HR'),
+(3, 'Charlie', 55000, 'HR'),
+(4, 'David', 75000, 'Finance'),
+(5, 'Eve', 80000, 'Finance'),
+(6, 'Frank', 72000, 'Finance'),
+(7, 'Grace', 90000, 'IT'),
+(8, 'Heidi', 95000, 'IT'),
+(9, 'Ivan', 87000, 'IT');
+
+-- FIRST VALUE FUNCION 
+select * from EmployeeSalaries
+
+select *
+,FIRST_VALUE(Salary) over(order by salary asc) [Minimum Salary]
+from EmployeeSalaries
+
+select *
+,FIRST_VALUE(EmployeeName) over(order by salary asc) [Emp with Minimum Salary]
+,FIRST_VALUE(Salary) over(order by salary asc) [Minimum Salary]
+from EmployeeSalaries
+
+
+select * 
+,FIRST_VALUE(EmployeeID) over(partition by department order by salary) [First Value]
+from EmployeeSalaries
+
+select * 
+,FIRST_VALUE(EmployeeID) over(partition by department order by salary desc) [First Value EID]
+,FIRST_VALUE(EmployeeName) over(partition by department order by salary desc) [First Value EName]
+from EmployeeSalaries
+
+
+-- LAST VALUE FUNCTION 
+
+select * from EmployeeSalaries
+
+--Incorrect Query
+select * 
+,LAST_VALUE(EmployeeName) over(order by salary desc) [Emp with Lowest Salary]
+from EmployeeSalaries
+
+
+--correct Query
+select * 
+,LAST_VALUE(EmployeeName) over(order by salary desc rows between unbounded preceding and unbounded following) [Emp with Lowest Salary]
+from EmployeeSalaries
+
+
+--correct Query
+select * 
+,LAST_VALUE(EmployeeName) over(order by salary desc rows between unbounded preceding and unbounded following) [Emp with Lowest Salary]
+,LAST_VALUE(Salary) over(order by salary desc rows between unbounded preceding and unbounded following) [Lowest Salary]
+from EmployeeSalaries
+
+--correct Query
+select * 
+,LAST_VALUE(EmployeeName) over(partition by department
+order by salary desc rows between unbounded preceding and unbounded following) [Emp with Lowest Salary]
+
+,LAST_VALUE(Salary) over(partition by department order by salary desc rows between unbounded preceding and unbounded following) [Lowest Salary]
+from EmployeeSalaries
+
+--correct Query [Last Value Window Fun]
+select * 
+,LAST_VALUE(EmployeeName) over(partition by department
+order by salary rows between unbounded preceding and unbounded following) [Emp with Highest Salary]
+
+,LAST_VALUE(Salary) over(partition by department order by salary asc rows between unbounded preceding and unbounded following) [Highest Salary]
+from EmployeeSalaries
+
+--correct Query [First Value Window Fun]
+select * 
+,first_VALUE(EmployeeName) over(partition by department
+order by salary desc) [Emp with Highest Salary]
+
+,first_VALUE(Salary) over(partition by department order by salary desc) [Highest Salary]
+from EmployeeSalaries
